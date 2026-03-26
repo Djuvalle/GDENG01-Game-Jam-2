e8b4090af315debc5d48b3d769b8554bdb45fbca
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2026-03-26 16:02:37 +0800

    Added coroutine HandleTerrainDestruction and rearranged constants

--- Assets/Scripts/GameSystems/TerrainGenerator.cs
+++ Assets/Scripts/GameSystems/TerrainGenerator.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 using GameEnum;
+using System.Collections.Generic;
+using System.Collections;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    private const float MIN_SPAWN_RANGE = 10;
-    private const string TERRAIN_PATH = "Prefabs/Terrain/";
+    private const float MIN_SPAWN_RANGE = 50;
+    private const float DESTROY_START_DELAY = 5f;
+    private const float SLOW_DESTROY_TIME = 5;
+    private const float MAX__SLOW_DESTROY_SIZE = 10;
+    
+    
     private static GameObject TerrainContainer;
     private static GameObject[] TerrainPrefabs;
+    private static Queue<GameObject> SpawnedTerrainQueue = new Queue<GameObject>();
     private static Vector3 PlayerPosition = Vector3.zero;
     [SerializeField] private GameObject StartingTerrain;
     private GameObject PrevEndObj;
@@ -34,6 +41,7 @@ public class TerrainGenerator : MonoBehaviour
                 TerrainContainer.transform
             );
             this.PrevEndObj = newTerrain.transform.Find("EndPoint").gameObject;
+            SpawnedTerrainQueue.Enqueue(newTerrain);
 
             //TODO: Broadcast new terrain added and add old terrain removal
         }
@@ -48,15 +56,48 @@ public class TerrainGenerator : MonoBehaviour
         PlayerPosition = new Vector3(x, y, z);
         this.TryGeneration();
     }
+
+    IEnumerator HandleTerrainDestruction()
+    {
+        yield return new WaitForSeconds(DESTROY_START_DELAY);
+        Debug.Log("Destroy loop started");
+
+        while (true)
+        {
+            if (SpawnedTerrainQueue.Count == 0)
+                yield return new WaitUntil(() => SpawnedTerrainQueue.Count > 0);
+                
+            GameObject terrain = SpawnedTerrainQueue.Dequeue();
+
+            // Additional code here
+
+            if (SpawnedTerrainQueue.Count >= MAX__SLOW_DESTROY_SIZE)
+            {
+                Debug.Log("Quick destroy terrain");
+                yield return null;
+            } else {
+                Debug.Log("Slow destroy terrain");
+                yield return new WaitForSeconds(SLOW_DESTROY_TIME);
+            }
+                
+            
+            if (terrain != null)
+                Destroy(terrain);
+        }
+    }
+
     private void Start()
     {   
+        const string TERRAIN_PATH = "Prefabs/Terrain/";
         TerrainContainer = GameObject.Find("TerrainContainer");
         TerrainPrefabs = Resources.LoadAll<GameObject>(TERRAIN_PATH);
         Debug.Log($"Number of terrain prefabs found: {TerrainPrefabs.Length}");
 
         this.PrevEndObj = StartingTerrain.transform.Find("EndPoint").gameObject;
+        SpawnedTerrainQueue.Enqueue(StartingTerrain);
         //this.PostEventWaypoint(PrevEndObj);
 
         EventBroadcaster.Instance.AddObserver(Notifications.PlayerPositionChanged.ToString(), this.HandlePlayerPositionChanged);
+        StartCoroutine(HandleTerrainDestruction());
     }
 }
