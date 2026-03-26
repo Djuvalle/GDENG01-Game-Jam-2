3014635a8a9d3ca0cd3bf65a86fcd49a6d1e362c
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2026-03-26 21:38:09 +0800

    Modified to fix camera from breaking while on car

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -3,26 +3,33 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    private const float HORIZONTAL_OFFSET = 5f;
-    private const float VERTICAL_OFFSET = 1.25f;
+    private const float HORIZONTAL_OFFSET = 8f;
+    private const float VERTICAL_OFFSET = 1.5f;
     [SerializeField]
     private float sensitivity;
     private Vector2 mouseInput;
     private float pitch;
+    private float yaw;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        
+        yaw = transform.eulerAngles.y;
     }
 
     private void LateUpdate()
     {
-        transform.Rotate(Vector3.up, mouseInput.x * sensitivity * Time.deltaTime);
+        yaw += mouseInput.x * sensitivity * Time.deltaTime;
         pitch -= mouseInput.y * sensitivity * Time.deltaTime;
-        pitch = Mathf.Clamp(pitch, -90f, 90f);
-        transform.localEulerAngles = new Vector3(pitch, transform.localEulerAngles.y, 0f);
-        transform.localPosition = transform.forward * -HORIZONTAL_OFFSET + new Vector3(0, VERTICAL_OFFSET);
+        pitch = Mathf.Clamp(pitch, -30f, 60f); // clamp for better vehicle viewing
+        
+        Quaternion targetRotation = Quaternion.Euler(pitch, yaw, 0f);
+        transform.rotation = targetRotation;
+
+        Vector3 offsetVector = targetRotation * Vector3.forward * -HORIZONTAL_OFFSET;
+        transform.position = transform.parent.position + offsetVector + new Vector3(0, VERTICAL_OFFSET, 0);
     }
 
     private void OnMouseMove(InputValue value)
