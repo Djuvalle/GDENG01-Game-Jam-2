0cc5739a2ecfd446c6502149fab481bf52bc2758
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2026-03-30 21:55:03 +0800

    Added Particle effect

--- Assets/Scripts/Controllers/CoinController.cs
+++ Assets/Scripts/Controllers/CoinController.cs
@@ -1,19 +1,26 @@
 using UnityEngine;
 using GameEnum;
+using System.Collections;
 
 public class CoinController : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem CollectEffect;
     private const float ROTATION_SPEED = 100f;
     private bool IsCollected = false;
+    IEnumerator DestroyAfterEffect()
+    {
+        yield return new WaitForSeconds(CollectEffect.main.startLifetime.constant);
+        Destroy(this.gameObject);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (IsCollected && !other.CompareTag("Player"))
             return;
 
         IsCollected = true;
-        // TODO: Add particles here
+        CollectEffect.Emit(1);
         EventBroadcaster.Instance.PostEvent(Notifications.CoinCollected.ToString());
-        Destroy(this.gameObject);
+        StartCoroutine(DestroyAfterEffect());
     }
 
     private void Update()
