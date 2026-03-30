75ad0ab6ca9c1e4ebca9bad9777f7d2b792a55b5
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2026-03-30 23:58:11 +0800

    Created CanvasController.cs

---
+++ Assets/Scripts/Controllers/CanvasController.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using GameEnum;
+using UnityEngine;
+using System;
+using DG.Tweening;
+
+public class CanvasController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI ScoreText;
+    [SerializeField] private RectTransform SpeedometerTicker;
+    [SerializeField] private GameObject GameOverMenu;
+
+    private void Start()
+    {
+        EventBroadcaster.Instance.AddObserver(Notifications.ScoreUpdated.ToString(), (param) =>
+        {
+            int score = param.GetIntExtra(ParameterKey.Score.ToString(), 0);
+            ScoreText.text = $"Score: {score:000,000,000}";
+        });
+        EventBroadcaster.Instance.AddObserver(Notifications.PlayerPositionChanged.ToString(), (param) =>
+        {
+            const float MAX_SPEED = 30f;
+            float speed = param.GetFloatExtra(ParameterKey.Speed.ToString(), 0);
+            float zRotation = 90 - (speed / MAX_SPEED) * 180;
+            SpeedometerTicker.DOLocalRotate(new Vector3(0, 0, zRotation), 0.1f).SetEase(Ease.OutSine);
+        });
+    }
+    private void OnDestroy()
+    {
+
+    }
+    private void ShowGameOverMenu()
+    {
+        GameOverMenu.SetActive(true);
+    }
+}
