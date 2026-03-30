5573e404cdd770ce8f6dcfef72016f7576689192
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2026-03-30 23:58:01 +0800

    Modified camera to smoothly follow

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,10 +1,12 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     private const float HORIZONTAL_OFFSET = 8f;
-    private const float VERTICAL_OFFSET = 1.5f;
+    private const float VERTICAL_OFFSET = 6f; //1.5f;
+    private GameObject Player;
     [SerializeField]
     private float sensitivity;
     private Vector2 mouseInput;
@@ -17,21 +19,22 @@ public class CameraController : MonoBehaviour
         Cursor.visible = false;
         
         yaw = transform.eulerAngles.y;
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void FixedUpdate()
     {
         //yaw += mouseInput.x * sensitivity * Time.deltaTime;
         //pitch -= mouseInput.y * sensitivity * Time.deltaTime;
-        yaw = transform.eulerAngles.y;
+        yaw = Player.transform.eulerAngles.y;
         pitch = 30f;
         pitch = Mathf.Clamp(pitch, -30f, 60f); // clamp for better vehicle viewing
         
         Quaternion targetRotation = Quaternion.Euler(pitch, yaw, 0f);
-        transform.rotation = targetRotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.fixedDeltaTime);
 
-        Vector3 offsetVector = targetRotation * Vector3.forward * -HORIZONTAL_OFFSET;
-        transform.position = transform.parent.position + offsetVector + new Vector3(0, VERTICAL_OFFSET, 0);
+        Vector3 offsetVector = /*targetRotation **/ Player.transform.forward * -HORIZONTAL_OFFSET;
+        transform.position = Player.transform.position + offsetVector + new Vector3(0, VERTICAL_OFFSET, 0);
     }
 
     private void OnMouseMove(InputValue value)
